3f7183fc99968ab49b736d10cfc127fe7613b3d1
dr-basalt <contact@ori3com.cloud>
2026-05-04 19:33:47 +0000

    debug: probe _collections schema

--- pocketbase/pb_migrations/1715000000_init_collections.js
+++ pocketbase/pb_migrations/1715000000_init_collections.js
@@ -1,11 +1,9 @@
 /// <reference path="../pb_data/types.d.ts" />
 migrate((app) => {
-  // Lister toutes les propriétés/méthodes accessibles sur app
-  const props = []
-  for (const key in app) {
-    props.push(key + ':' + typeof app[key])
-  }
-  // Aussi tenter Object.keys
-  try { props.push('KEYS:' + Object.keys(app).join(',')) } catch(e) {}
-  throw new Error('APP_PROPS: ' + props.join('|'))
+  // Probe: afficher le CREATE TABLE pour _collections
+  const info = []
+  app.newQuery("SELECT name, sql FROM sqlite_master WHERE type='table' AND name LIKE '\\_\\_%' ESCAPE '\\'").each((r) => {
+    info.push(r.name + ': ' + (r.sql || '').substring(0, 300))
+  })
+  throw new Error('SCHEMA: ' + info.join(' ||| '))
 }, (_) => {})
